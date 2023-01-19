d0c346d5394c6841464fe6271dc6aa4dcff92ea6
Aleksey Korshuk <48794610+AlekseyKorshuk@users.noreply.github.com>
2023-01-19 17:34:57 +0300

    Update accelerator.py

--- src/accelerate/accelerator.py
+++ src/accelerate/accelerator.py
@@ -98,7 +98,7 @@ if is_tpu_available(check_device=False):
     import torch_xla.distributed.xla_multiprocessing as xmp
 
 
-if is_torch_version("<=", "1.13.5"):
+if is_torch_version("<=", "1.13.5") or is_torch_version("==", "1.14.0a0+410ce96"):
     from torch.optim.lr_scheduler import _LRScheduler as LRScheduler
 else:
     from torch.optim.lr_scheduler import LRScheduler as LRScheduler
